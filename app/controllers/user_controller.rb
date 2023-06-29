1b1cbf23e4674774487b26f852eef248fc34d760
kevin-wairi <kevinwairi@gmail.com>
2023-06-29 05:23:03 +0300

    assignment completed

---
+++ app/controllers/user_controller.rb
@@ -0,0 +1,26 @@
+class UserController < ApplicationController
+  def create
+    user = User.create(user_params)
+    if user.valid?
+      session[:user_id] = user.id
+      render json: user, status: :created
+     else
+      render json: {errors: user.errors.full_messages},status: :unprocessable_entity
+    end
+  end
+
+  def show
+    if session[:user_id]
+      user = User.find_by(id: session[:user_id])
+      render json: user,status: :ok if user
+     else
+      render json: {error: 'unauthorized'},status: :unauthorized
+    end
+  end
+
+
+  private
+  def user_params
+    params.permit(:username,:password,:password_confirmation)  
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  get 'sessions/create'
+  get 'sessions/destroy'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+  post '/signup', to: 'user#create'
+  get '/me', to: 'user#show' 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
